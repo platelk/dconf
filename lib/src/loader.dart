75fee97505353c6bde5a9d567973ef6566f0ced3
PLATEL Kévin <platel.kevin@gmail.com>
2019-04-09 08:37:32 +0200

    Load from content

--- lib/src/loader.dart
+++ lib/src/loader.dart
@@ -6,18 +6,21 @@ import './config.dart';
 /// [Loader] is an abstract class which will contains the retrieve of the configuration
 /// from the defined sources, apply it on specific order to have a resolved configuration
 abstract class Loader {
-	String _configName;
-	String _defaultConfigType;
-	Map<String, dynamic Function(String)> _parsers = {};
+	String _configName = "config";
+	String _defaultConfigType = "yaml";
+	Map<String, dynamic Function(String)> parsers = {};
 	
 	Loader() {
-		this._parsers["yaml"] = loadYaml;
-		this._parsers["json"] = json.decode;
+		this.parsers["yaml"] = (String content) {
+			var v = loadYaml(content);
+			return json.decode(json.encode(v));
+		};
+			this.parsers["json"] = json.decode;
 	}
 	
 	Config load();
 	Config loadFrom(String content) {
-		return new Config.fromMap(_parsers[_defaultConfigType](content));
+		return new Config.fromMap(parsers[_defaultConfigType](content));
 	}
 	set configType(String name) => _defaultConfigType = name.toLowerCase();
 	set configName(String name) => _configName = name;
--- test/loader_test.dart
+++ test/loader_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
 	group('loader loadFrom', () {
-		test('parse json', () {
+		test('parse yaml', () {
 			var loader = new conf.IOLoader();
 			loader.configType = "yaml";
 			var c = loader.loadFrom("""
@@ -12,6 +12,21 @@ debug: true
 user:
   firstname: "john"
   lastname: "doe"
+""");
+			expect(c["debug"], isTrue);
+			expect(c["user.firstname"], equals("john"));
+		});
+		test('parse json', () {
+			var loader = new conf.IOLoader();
+			loader.configType = "json";
+			var c = loader.loadFrom("""
+{
+	"debug": true,
+	"user": {
+		"firstname": "john",
+		"lastname": "doe"
+	}
+}
 """);
 			expect(c["debug"], isTrue);
 			expect(c["user.firstname"], equals("john"));
